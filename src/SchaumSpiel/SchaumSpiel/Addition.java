a8417a6cc1f771c7bb603f9f5d54ef3073a61555
xion13 <Raffael-Elias.Beyer@stud.hshl.de>
2017-06-23 13:23:26 +0200

    Add files via upload

---
+++ src/SchaumSpiel/SchaumSpiel/Addition.java
@@ -0,0 +1,14 @@
+/*package SchaumSpiel;
+
+import java.rmi.*;
+import java.rmi.server.*;
+
+public class  extends UnicastRemoteObject implements Interface {
+
+	public () throws RemoteException {
+	}
+
+	public int add(int a, int b) throws RemoteException {
+		
+	}
+}*/
---
+++ src/SchaumSpiel/SchaumSpiel/test.java
@@ -0,0 +1,29 @@
+/*package SchaumSpiel;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Spielfeld feld = new Spielfeld(); // WICHTIG!!!
+		int sc = feld.sc;
+		int st = feld.st;
+		int pa = feld.pa;
+
+		System.out.println(sc);
+		System.out.println(st);
+		System.out.println(pa);
+
+		sc = feld.sc;
+		st = feld.st;
+		pa = feld.pa;
+
+		while (sc == 0 ^ st == 1 ^ pa == 2) {
+			System.out.println(sc);
+			System.out.println(st);
+			System.out.println(pa);
+		}
+
+		System.out.println("Ende");
+	}
+
+}*/
